45c37ed07882ca5f6f85c07e8df4785e2944dac6
Sam Olaogun <sam.olaogun@gmail.com>
2017-06-23 18:42:34 -0400

    Added .babelrc

--- webpack.common.js
+++ webpack.common.js
@@ -16,12 +16,7 @@ module.exports = {
         rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015']
-                }
-            }
+            loader: 'babel-loader',
         }, {
             test: /\.css$/,
             use: ExtractTextPlugin.extract({
