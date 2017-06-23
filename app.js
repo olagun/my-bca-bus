f78b354fdc5a9f43ba8ab05c68c80935d56c5cc5
Sam Olaogun <sam.olaogun@gmail.com>
2017-06-23 17:04:21 -0400

    Added production files

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('dotenv');
+const PORT = process.env.PORT || 80;
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, 'build/static')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
+
+module.exports = app;
