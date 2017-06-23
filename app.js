0cf430469fd0c36e8d030587c087033f9eef8c24
Sam Olaogun <sam.olaogun@gmail.com>
2017-06-23 12:32:39 -0400

    Stable 1.1

--- app.js
+++ app.js
@@ -1,81 +1,136 @@
 /**
  * MyBCABus
  * 
- * @since 1.0
- * @author Sam Olaogun
- * @description MyBCABus refactored to be more efficient and maintainable
+ * MyBCABus refactored to be more efficient and maintainable
  * IE (both mobile and desktop) and Opera mobile do not support the Fetch API
+ * 
+ * @since 1.0
+ * @version 1.1
+ * @author Sam Olaogun
  */
-'use strict'
+'use strict';
 
-// Public Key
-const key = '1tJllDysWV5Xn9C7MKlVDttPXp2jXuQCYLP3jbf4FW28'
-const url = `https://spreadsheets.google.com/feeds/list/${key}/od6/public/values?alt=json`
-
-const input = document.querySelector('.town__search')
-const townContainer = document.querySelector('.town__container')
-const towns = {}
-
-const favoriteContainer = document.querySelector('.favorite__container')
-let favorite = localStorage.getItem('favorite')
-
-
+// PUBLIC
+const KEY = '1tJllDysWV5Xn9C7MKlVDttPXp2jXuQCYLP3jbf4FW28';
+const SPREADSHEET_URL = `https://spreadsheets.google.com/feeds/list/${KEY}/od6/public/values?alt=json`;
 
 /**
- * Recursively updates the view
- * 
- * @constructor updateView
- * @param start {boolean} First mount
- * @param url {string} Google Spreadsheets API url
+ * @class       MyBCABus
+ * @description Recursively updates the view and polls for 
+ *              new spreadsheet data.
  */
-(function updateView(start, url) {
-    fetch(url)
-        .then(data => data.json()
-            .then(data => {
-                data.feed.entry.forEach(entry => {
-                    if ('gsx$_cre1l' in entry && 'gsx$townsbuslocation_2' in entry)
-                        towns[entry.gsx$townsbuslocation_2.$t] = entry.gsx$_cre1l.$t
+class MyBCABus {
+    constructor() {
+        this.firstRender = true;
+        this.towns = {};
+        this.inputElement = document.querySelector('.town__search');
+        this.favoriteContainer = document.querySelector('.favorite__container');
+        this.townContainer = document.querySelector('.town__container');
 
-                    if ('gsx$_cokwr' in entry && 'gsx$townsbuslocation' in entry)
-                        towns[entry.gsx$townsbuslocation.$t] = entry.gsx$_cokwr.$t
-                })
+        // Begin local long-polling cycle
+        this._poll();
+    }
 
-                // Mount only the first update
-                if (start) {
-                    Object.keys(towns).forEach(town =>
-                        // Perf mount
-                        townContainer.insertAdjacentHTML('beforeend', `
-                            <div 
-                                class="town"
-                                data-town="${town}"
-                                onClick="handleFavorite()"
-                            >
-                                <span class="town__name">${town}</span>
-                                <span class="town__loc">${towns[town]}</span>
-                            </div>
-                        `)
-                    )
-                } else {
-                    document.querySelectorAll('.town').forEach(town =>
-                        town.children[1].textContent = towns[town.dataset.town])
+    get favorite() {
+        return this.favoriteElement || void 0;
+    }
 
-                    input.addEventListener('input', () => {
-                        // TODO: Change route to match query and push to history
-                        document.querySelectorAll(`.town`).forEach(town =>
-                            town.style.display = 'flex')
+    set favorite(element) {
+        this.favoriteElement = element;
 
-                        if (input.value)
-                            document.querySelectorAll(`.town:not([data-town*="${input.value}" i])`).forEach(town =>
-                                town.style.display = 'none')
-                    })
-                }
+        if ('Storage' in window)
+            localStorage.setItem('favorite', this.favoriteElement.dataset.town);
+    }
 
-                updateView(false, url)
-            })
-        ).catch(() => document.write('No buses are here.'))
-})(true, url)
+    /**
+     * @description Pull data from the spreadsheet on each render.
+     * @async
+     * @protected
+     */
+    async _poll() {
+        const data = await fetch(SPREADSHEET_URL);
+        const json = await data.json();
 
-// TODO: Implement App Shell (Service Worker)
+        // Populate towns
+        json.feed.entry.forEach(entry => {
+            if ('gsx$_cre1l' in entry && 'gsx$townsbuslocation_2' in entry)
+                this.towns[entry.gsx$townsbuslocation_2.$t] = entry.gsx$_cre1l.$t;
+
+            if ('gsx$_cokwr' in entry && 'gsx$townsbuslocation' in entry)
+                this.towns[entry.gsx$townsbuslocation.$t] = entry.gsx$_cokwr.$t;
+        });
+
+        this._render();
+        this._poll();
+    }
+
+    /**
+     * @description Render the application state.
+     * @protected
+     */
+    _render() {
+        // PERF: Mount only on first render
+        if (this.firstRender) {
+            this.firstRender = false;
+            Object.keys(this.towns).forEach(this._createTown, this);
+        } else {
+            document.querySelectorAll('.town').forEach(town =>
+                town.children[1].textContent = this.towns[town.dataset.town]);
+
+            this.inputElement.addEventListener('input', this._handleInput);
+        }
+    }
+
+    /**
+     * @description Handles search field value changes.
+     * @callback    _handleInput
+     * @protected
+     */
+    _handleInput() {
+
+        document.querySelectorAll(`.town`).forEach(town =>
+            town.style.display = 'flex');
+
+        // Cheaply hide irrelevant towns
+        // TODO: Change route to match query and push to history
+        if (this.inputElement.value)
+            document.querySelectorAll(`.town:not([data-town*="${input.value}" i])`).forEach(town =>
+                town.style.display = 'none');
+    }
+
+    /**
+     * @description               Creates town element on first render.
+     * @param       {string} town the town name
+     * @protected
+     */
+    _createTown(town) {
+        const townElement = document.createElement('div');
+
+        townElement.classList.add('town');
+        townElement.dataset.town = town;
+        townElement.insertAdjacentHTML('beforeend', `
+            <span class="town__name">${town}</span>
+            <span class="town__loc">${this.towns[town]}</span>
+        `);
+
+        townElement.addEventListener('click', _ => {
+            if (this.favoriteElement) {
+                this.favoriteContainer.removeChild(this.favoriteElement);
+                this.townContainer.insertAdjacentElement('beforeend', this.favoriteElement);
+            }
+
+            this.favoriteContainer.insertAdjacentElement('beforeend', this.favoriteElement = townElement);
+        });
+
+        if (this.favorite === town)
+            this.favoriteContainer.insertAdjacentElement('beforeend', this.favorite = townElement);
+        else
+            this.townContainer.insertAdjacentElement('beforeend', townElement);
+    }
+};
+
+window.addEventListener('DOMContentLoaded', _ => new MyBCABus);
+
+// TODO: Implement App Shell & Service Worker
 // TODO: Configure Webhook
 // TODO: Configure Routers
-// TODO: Tail-call test
